334feb616db95bff13abe34a1641e02ec4852346
wangmi <wx_b4b04e4419604d908b128340b7d1e8cf@git.code.tencent.com>
2020-10-09 17:59:32 +0800

    完善和修复状态机实现

--- src/test/java/cn/kunm/whirlwind/ProcessTest.java
+++ src/test/java/cn/kunm/whirlwind/ProcessTest.java
@@ -28,57 +28,31 @@ public class ProcessTest {
         Status initStatus = new Status("start", true, false);
         // 中间状态
         Status midAStatus = new Status("midA");
-        Status midA1Status = new Status("midA1");
         Status midBStatus = new Status("midB");
-        Status midB1Status = new Status("midB1");
-        Status midB2Status = new Status("midB2");
         Status midCStatus = new Status("midC");
-        Status midC1Status = new Status("midC1");
-        Status midC2Status = new Status("midC2");
-        Status midC3Status = new Status("midC3");
         // 终止状态
         Status endStatus = new Status("end", false, true);
 
         // 状态流转
-        initStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midAStatus, midBStatus, midCStatus)));
-        midAStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midA1Status)));
-        midBStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midB1Status, midB2Status)));
-        midCStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midC1Status, midC2Status, midC3Status)));
+        initStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midAStatus)));
+        midAStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midBStatus)));
+        midBStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(midCStatus)));
+        // 流转到终止状态的from状态可以不显式声明
+        // midCStatus.setAllowNextStatus(new HashSet<>(Arrays.asList(endStatus)));
 
-        allStatus.addAll(Arrays.asList(initStatus, midAStatus, midA1Status, midBStatus, midB1Status, midB2Status,
-                midCStatus, midC1Status, midC2Status, midC3Status, endStatus));
-
+        allStatus.addAll(Arrays.asList(initStatus, midAStatus, midBStatus, midCStatus, endStatus));
 
         StatusMachine sm = new StatusMachine(allStatus);
         sm.start();
 
-        // Event eS = new Event("es", "start", "midA", new HS());
-        // Event eA = new Event("ea", "midA", "midA1", new HA());
-        // Event eAE = new Event("ea", "midA1", "end", new HE());
-        // sm.sendEvent(eS);
-        // sm.sendEvent(eA);
-        // sm.sendEvent(eAE);
-
-        Event eS = new Event("es", "start", "midB", new HS());
-        Event eB1 = new Event("eb1", "midB", "midB1", new HB());
-        // Event eB2 = new Event("eb2", "midB1", "midB2", new HB());
-        Event eBE = new Event("ebe", "midB1", "end", new HE());
-        sm.sendEvent(eS);
-        sm.sendEvent(eB1);
-        // sm.sendEvent(eB2);
-        sm.sendEvent(eBE);
-
-
-        // Event eC1 = new Event("ec", "midC", "midC1", new HC());
-        // Event eC2 = new Event("ec", "midC", "midC2", new HC());
-        // Event eC3 = new Event("ec", "midC", "midC3", new HC());
-        // Event eE = new Event("ee", "midC3", "end", new HE());
-
-
-        // sm.sendEvent(eB1);
-        // sm.sendEvent(eS);
-        // sm.sendEvent(eS);
-        // sm.sendEvent(eS);
+        Event eA = new Event("ea", "start", "midA", new HS());
+        Event eB = new Event("eb", "midA", "midB", new HA());
+        Event eC = new Event("ec", "midB", "midC", new HE());
+        Event eE = new Event("ee", "midC", "end", new HE());
+        sm.sendEvent(eA);
+        sm.sendEvent(eB);
+        sm.sendEvent(eC);
+        sm.sendEvent(eE);
     }
 
 
